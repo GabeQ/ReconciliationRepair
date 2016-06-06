041ec587ff1c4cdc5cb63655ed2d701cbd74fbec
chenpekker <cssummer16@c74.clinic.cs.hmc.edu>
2016-06-06 10:23:08 -0700

    update

--- treeOrganizer.py
+++ treeOrganizer.py
@@ -1,3 +1,6 @@
+#takes tree from various files and puts them into a new directory
+# modified by Chen Pekker
+
 import shutil
 import os
 
@@ -5,10 +8,9 @@ import os
 for folderName, subfolders, filenames in os.walk('/Users/cssummer16/GitHub/ReconciliationRepair/mediumDTL'): #need to change this for every directory
     for filename in filenames:
         stringName = str(filename)
-        if ".tree" in stringName:
-            print "YESSSS"
+        if ".newick" in stringName:
             os.chdir(folderName)
-            shutil.copy(filename, "/Users/cssummer16/GitHub/ReconciliationRepair/mediumDTL/mediumOrganizedTrees") #need to change the output file
+            shutil.copy(filename, "/Users/cssummer16/GitHub/ReconciliationRepair/mediumDTL/mediumNewickTrees") #need to change the output file
         print('File inside: ' + folderName + ' <- of that folder: '+ filename)
 
     print('')
