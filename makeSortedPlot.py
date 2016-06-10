6b123da430995ec96e2c7bca8a80033f64e37d3a
chenpekker <gpekker@g.hmc.edu>
2016-06-10 15:05:59 -0700

    added memoization to janeSort

--- makeSortedPlot.py
+++ makeSortedPlot.py
@@ -3,7 +3,7 @@
 # modified from 'makePlot.py' by Chen Pekker
 # June 2016
 #
-# This file plots costs of temporally consistent reconciliations found 
+# This file plots the results of fixer and Jane into costs of temporally consistent reconciliations found 
 # by TemporalConsistencyFixer algorithm as well as those found by Jane 
 # against file indices.
 #
@@ -24,15 +24,16 @@ tVal = 3
 lVal = 1
 num = 100
 
-janeMemo = {}
+# memoization for jane, optimal, and fixer
+janeMemo = {} 
 optimalMemo = {}
 fixerMemo = {}
 
 def findGroups(fileNum):
-    
-        janeList = []
-	fixerList = []
-	optimalList = []
+        """takes in a list of samples numbers and returns three lists:
+        the first contains sample numbers of those who's optimal score was better than Jane's score.
+        The next returns a list of samples numbers who's fixer output < Jane.
+        Third return sample #s for those who's fixer score equals Jane's. """
 	
         optimal = [] #list of scores where the optimal score is better than Jane
         repair = [] #list of scores where the repair score is better than Jane
@@ -40,39 +41,28 @@ def findGroups(fileNum):
         
         scores = []
 	for i in fileNum:
-	    
 		index = str(i)
-	#	print "index B4: " + index
-		
-		for j in xrange(4 - len(str(i + 1))):
-			index = "0" + index
-		
-		f = "janeCosts/COG" + index + ".txt"
-
+	#	print "index before: " + index
+	#	for j in xrange(4 - len(str(i + 1))):
+	#		index = "0" + index
+		f = "janeCosts/G" + index + ".txt"
 		if not os.path.isfile(f):
 			continue
-
 		janeFile = open(f, 'r')
-
 		lines = janeFile.readlines()
-		janeList.append(int(lines[-1]))
 		janeScore = int(lines[-1])
 		janeMemo[i] = janeScore
 		print "Jane:", int(lines[-1])
-
 		janeFile.close()
-
-		f = "fixerOut/COG" + index + ".txt"
-
+		
+		f = "fixerOut/G" + index + ".txt"
 		fixerFile = open(f, 'r')
-
 		for line in fixerFile:
 			if line.startswith("min total:"):
 				if line.startswith("min total: None"):
-					fileName = "real-100taxa/COG" + index + ".newick"
+					fileName = "real-100taxa/G" + index + ".newick"
 					recs, allRecs = MasterReconciliation.Reconcile(["", fileName, str(dVal), str(tVal), str(lVal), "unit", "0", "1", "0", "1"])
 					T = allRecs[0]
-
 					d, s, t, l = 0, 0, 0, 0
 					for key in T.keys():
 						if T[key][0] == 'D':
@@ -83,27 +73,20 @@ def findGroups(fileNum):
 							t += 1
 						elif T[key][0] == 'L':
 							l += 1
-
 					score = d * dVal + t * tVal + l * lVal
 
-					optimalList.append(score)
 					print "Optimal:", score
 					optimalScore = score
 					optimalMemo[i] = optimalScore
 					fixerScore = None
-					#fixerMemo[i] = fixerScore
 				else:
-					fixerList.append(int(line[11:]))
 					print "Fixer:", int(line[11:])
 					fixerScore = int(line[11:])
 					fixerMemo[i] = fixerScore
 					optimalScore = None
-					#optimalMemo[i] = optimalScore
 		fixerFile.close()
 		print "Done ", index
 		scores.append((i, janeScore, optimalScore, fixerScore))
-		
-	print scores
 	for i in range(len(scores)):
 	    if scores[i][2] != None: #if there is an optimal score and a 'None' for the fixer score
 	        if scores[i][2] < scores[i][1]: #if the optimal score is less than the Jane score
@@ -111,76 +94,57 @@ def findGroups(fileNum):
 	    elif scores[i][3] != None: #if there is an fixer score and a 'None' for the optimal score
 	        if scores[i][3] < scores[i][1]: 
 	            repair.append(scores[i][0])
-	        if scores[i][3] == scores[i][2]:
+	        if scores[i][3] == scores[i][2]: #if the fixer score and Jane score are the same
 	            tie.append(scores[i][0])
-	           
-	print optimal, repair, tie
         return optimal, repair, tie
 
-
 def janeSort(list):
-        """ takes in a list with the index numbers of samples, 
+        """takes in a list with  index numbers of samples, 
         returns a list of the sample numbers ranked in order of 
         increasing jane scores for those samples"""
-    
-        optimal = [2,23,26,27,29,33,34,41,52,58,62,67,69,72,86,92]
-        repair = [1,16,24,30,51,55,61,75,85,96,98]
-        tie = [95]
-        allElse = []
-        for x in range(1,101):
-            if x not in optimal:
-                if x not in repair:
-                    if x not in tie:
-                        allElse.append(x)
-        fileNum = list
-            
+        fileNum = list  
         scores = []
 	for i in fileNum:
-	    
-		index = str(i)		
-		for j in xrange(4 - len(str(i + 1))):
-			index = "0" + index
-		
-		f = "janeCosts/COG" + index + ".txt"
-
+		index = str(i)
+		print "index B4: " + index
+	#	for j in xrange(4 - len(str(i + 1))):
+	#		index = "0" + index
+		f = "janeCosts/G" + index + ".txt"
 		if not os.path.isfile(f):
 			continue
 
-		janeFile = open(f, 'r')
-
-		lines = janeFile.readlines()
-		janeScore = int(lines[-1])
+		if i in janeMemo:
+		    janeScore = janeMemo[i]
+		else:
+		        print "NEEDED TO GO IN THE 'ELSE CASE' FOR JANE in janeSORT"
+		        janeFile = open(f, 'r')
+          		lines = janeFile.readlines()
+          		janeScore = int(lines[-1])
+		        janeFile.close()
+		        
 		scores.append((janeScore, i)) #list of tuples
-
-		janeFile.close()
-	
 	sortedScores = sorted(scores) #sorted list of tuples
 	janeIncreasing = []
 	for i in range(len(sortedScores)):
 	    janeIncreasing.append(sortedScores[i][1]) #get the sample numbers (second element in the tuples) from sortedScores
-	    
 	return janeIncreasing
 
-
+""" main method generates graph using above methods"""
 def main():
-    
 	janeX = []
 	fixerX = []
 	janeList = []
 	fixerList = []
 	optimalX = []
 	optimalList = []
-	
 	fileNum = range(1, num+1)
 	optimal, repair, tie = findGroups(fileNum)
-	
         remaining = []
         for x in range(1, num+1): 
             if x not in optimal:
                 if x not in repair:
                     if x not in tie:
                         remaining.append(x)
-                        
         optimal = janeSort(optimal)
         print optimal
         repair = janeSort(repair)
@@ -200,59 +164,43 @@ def main():
         for i in allElse:
             realFileNum.append(i)
             
-        print "realFileNum:"
-        print realFileNum
-            
         count = 0
-        
 	for i in realFileNum:
 	        count +=1
 	        janeX.append(count)
-
 		index = str(i)
 		print "index B4: " + index
-		
-		for j in xrange(4 - len(str(i + 1))):
-			index = "0" + index
-		
-		f = "janeCosts/COG" + index + ".txt"
-
+	#	for j in xrange(4 - len(str(i + 1))):
+	#		index = "0" + index
+		f = "janeCosts/G" + index + ".txt"
 		if not os.path.isfile(f):
 			continue
 
 		if i in janeMemo:
 		    janeList.append(janeMemo[i])
-		    
 		else:
 		        print "NEEDED TO GO IN THE 'ELSE CASE' FOR JANE"
 		        janeFile = open(f, 'r')
-
           		lines = janeFile.readlines()
           		janeList.append(int(lines[-1]))
           		print "Jane:", int(lines[-1])
-            
           		janeFile.close()
-          		
                 if i in fixerMemo:
                     fixerX.append(count)
 		    fixerList.append(fixerMemo[i])
-		
 		elif i in optimalMemo:
 		    optimalList.append(optimalMemo[i])
 		    optimalX.append(count)
 		else:
 		        print "NEEDED TO GO IN THE 'ELSE CASE' FOR FIXER/OPTIMAL"
-		        f = "fixerOut/COG" + index + ".txt"
-
+		        f = "fixerOut/G" + index + ".txt"
           		fixerFile = open(f, 'r')
-            
           		for line in fixerFile:
          			if line.startswith("min total:"):
             				if line.startswith("min total: None"):
-           					fileName = "real-100taxa/COG" + index + ".newick"
+           					fileName = "real-100taxa/G" + index + ".newick"
            					recs, allRecs = MasterReconciliation.Reconcile(["", fileName, str(dVal), str(tVal), str(lVal), "unit", "0", "1", "0", "1"])
            					T = allRecs[0]
-            
            					d, s, t, l = 0, 0, 0, 0
            					for key in T.keys():
           						if T[key][0] == 'D':
@@ -263,9 +211,7 @@ def main():
          							t += 1
           						elif T[key][0] == 'L':
          							l += 1
-            
            					score = d * dVal + t * tVal + l * lVal
-            
            					optimalList.append(score)
            					optimalX.append(count)
            					print "Optimal:", score
@@ -276,37 +222,22 @@ def main():
            					print "Fixer:", int(line[11:])
            					print "NEEDED TO GO IN for FIXER"
           		fixerFile.close()
-          		print "Done ", index
-        
-     
-      	print "janeX:"
-        print janeX
-        print janeList
-      	print "fixerX:"
-        print fixerX
-        print fixerList
-        print "optimalX:"
-        print optimalX
-        print optimalList
-        	     		    
+          		print "Done ", index	    
         fig, ax = plt.subplots()
         ax.set_xticks([16, 27, 100]) #ticks for the x axis
 	ax.plot(janeX, janeList, 'ro', label='Jane')
 	ax.plot(fixerX, fixerList, 'b*', label='Repair')
 	ax.plot(optimalX, optimalList, 'y^', label='No Repair')
-	plt.xlabel('COG File', fontsize=21)
+	plt.xlabel('G File', fontsize=21)
 	plt.ylabel('Cost', fontsize=21)
 	plt.legend()
 	plt.grid()
 	#plt.xticks(np.arange(0, count, 100))
 	legend = ax.legend(loc='upper right', numpoints = 1)
-	
 	# Set the fontsize
         for label in legend.get_texts():
             label.set_fontsize('xx-large')
-            
         plt.show()
-
 
 if __name__ == "__main__":
 	main()
